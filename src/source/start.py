4b58f426909a658a8de245f2e5187a6a5e78282a
Priyansh #CrazyBoy <106391059+PriyanshGoswami@users.noreply.github.com>
2023-02-09 22:30:40 +0530

    Update start.py

--- src/source/start.py
+++ src/source/start.py
@@ -8,11 +8,11 @@ from pyrogram import __version__ as pyrover
 from src.events import register
 from src import telethn as tbot
 
-VENOM = "https://telegra.ph/file/7c97605753018dfa4f832.mp4"
+VENOM = "https://telegra.ph/file/3d67ebcbd3a6373946a82.mp4"
 
 @register(pattern=("/start"))
 async def awake(event):
-  TEXT = f"Heya [{event.sender.first_name}](tg://user?id={event.sender.id}), My name is Emilia - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.\n\n"
-  TEXT += "Join my [News Channel](t.me/PikachuUpdates) to get information on all the latest updates."
+  TEXT = f"Heya [{event.sender.first_name}](tg://user?id={event.sender.id}), My name is Emilia - I'm an Animed themed Group Management Bot, Here to help you manage your groups! Use /help to know about my Full Potential.\n\n"
+  TEXT += "Join my [Updates Channel](t.me/PikachuUpdates) to get information on all the latest updates."
   BUTTON = [[Button.url("➕Add Me To Your Group➕", "https://t.me/EmiliaProBot?startgroup=true"),]]
   await tbot.send_file(event.chat_id, VENOM, caption=TEXT, buttons=BUTTON)
